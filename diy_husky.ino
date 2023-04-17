50e409d67085acac75645bc2f0afe7dd0aa671b6
PerCaholic23 <88301061+PerCaholic23@users.noreply.github.com>
2023-04-17 11:11:54 +0700

    Update diy_husky.ino

--- diy_husky.ino
+++ diy_husky.ino
@@ -5,14 +5,15 @@
 #include <pop7.h>
 #include <SoftwareSerial.h>
 #include "HUSKYLENS.h"
+
+
 SoftwareSerial mySerial(30, 29); //rx,tx
 HUSKYLENS on; //declare varible instead of HUSKYLENS
-int fullyDimension = 9450;
+int fullyDimension = 19881;
 int total_package = 0, failed_package = 0, passed_package = 0;
+int del = 550, del_m = 1500;
 
-float dim;
-int index;
-bool sdChk;
+float dim, dim_backup;
 
 void printResult(HUSKYLENSResult result);
 void printResultF(HUSKYLENSResult result);
@@ -31,9 +32,9 @@ void setup() {
   Serial.println(F("Successfully connected"));
   //F() is macro that will store a string in a flash momory instead of store in SRAM.
   //This is useful when we need to save memory
-
   on.writeAlgorithm(ALGORITHM_COLOR_RECOGNITION); //fixed algorithm in huskylens
-  on.setCustomName("Side dish", 1); on.setCustomName("Rice", 2); on.setCustomName("Error", 3);
+  on.setCustomName("Pizza", 1); on.setCustomName("Error", 2);
+  servo(1, 90);
 }
 
 void loop() {
@@ -45,27 +46,33 @@ void loop() {
   glcd(2, 10, "Pass= %d ", passed_package);
   setTextColor(GLCD_WHITE);
 
-  while (read_ir(A0) > 12) {
-    total_package += 1;
-    motor(1, 50);
+  while (on.countBlocks() == 0) {
+    on.requestBlocks();
+    motor(4, -65);
   }
-  motor(1, 50);
-  delay(250);
-  ao();
+
   if (on.requestBlocks()) {
     if (on.countBlocks(1) == 1) {
-      glcd(5, 0, "Rice detected!     ");
-      sdChk = false;
-      if (on.countBlocks(3) == 1) {
+      glcd(5, 0, "Detected!                ");
+      if (on.countBlocks(2) == 1) {
+        total_package += 1;
+        delay(del);
+        motor(4, 0);
+        delay(del);
+        on.requestBlocks();
         setTextColor(GLCD_RED);
         glcd(5, 19, "Error!");
-        index = 1;
-        for (int i = 0; i < on.countBlocks(3); i++) {
-          HUSKYLENSResult result = on.get(3, i);
+        for (int i = 0; i < on.countBlocks(2); i++) {
+          HUSKYLENSResult result = on.get(2, i);
           printResultF(result);
           delay(300);
         }
       } else {
+        total_package += 1;
+        delay(del);
+        motor(4, 0);
+        delay(del);
+        on.requestBlocks();
         for (int i = 0; i < on.countBlocks(1); i++) {
           HUSKYLENSResult result = on.get(1, i);
           printResult(result);
@@ -74,29 +81,9 @@ void loop() {
       }
     }
 
-    if (on.countBlocks(2) == 1) {
-      glcd(5, 0, "Side dish detected!     ");
-      sdChk = true;
-      if (on.countBlocks(3) == 1) {
-        setTextColor(GLCD_RED);
-        glcd(5, 19, "Error!");
-        index = 2;
-        for (int i = 0; i < on.countBlocks(3); i++) {
-          HUSKYLENSResult result = on.get(3, i);
-          printResultF(result);
-          delay(300);
-        }
-      } else {
-        for (int i = 0; i < on.countBlocks(2); i++) {
-          HUSKYLENSResult result = on.get(2, i);
-          printResult(result);
-          delay(300);
-        }
-      }
-    }
-
-    if (on.countBlocks(1) == 0 && on.countBlocks(2) == 0 && on.countBlocks(3) == 0) {
-      glcd(5, 0, "Waiting for packages...");
+    if (on.countBlocks(1) == 0 && on.countBlocks(2) == 0) {
+      mySerial.write('W');
+      glcd(5, 0, "Waiting for packages...        ");
       glcd(6, 0, "                         ");
       glcd(7, 0, "                         ");
     }
@@ -108,41 +95,40 @@ void printResult(HUSKYLENSResult result) {
   glcd(1, 0, "Y = %d ", result.yCenter);
   glcd(2, 0, "W = %d ", result.height);
   glcd(3, 0, "H = %d ", result.width);
-  dim = result.width * result.height;
-  Serial.println(dim);
-  glcd(6, 0, "Dimension= %f ", dim);
-  float percentage = (dim / fullyDimension) * 100;
+  dim_backup = result.width * result.height;
+  glcd(6, 0, "Dimension= %f ", dim_backup);
+  float percentage = (dim_backup / fullyDimension) * 100;
   glcd(7, 0, "Integrity= %f% ", percentage);
   //on.setTimeOutDuration(10000); //millisecond format
   if (percentage > 80) {
+    swnd('P');
     Serial.print("Pass");
     mySerial.write('P');
-    swnd('P');
-    if (sdChk == true) {
-      passed_package += 1;
-      //move package into passed storage
-      while (read_ir(A1) > 12) {
-        motor(1, 50);
-      }
-      //move package into passed storage
+    passed_package += 1;
+    while (read_ir(A2) > 12) {
+      motor(4, -50);
     }
+    delay(10);
+    motor(4, 0);
   } else {
-    Serial.print("Failed");
     swnd('F');
-    servo(1, 50);
-    delay(600);
+    Serial.print("Failed");
     mySerial.write('F');
     failed_package += 1;
-    //move this package into failed storage
     while (read_ir(A1) > 12) {
-      motor(1, 50);
+      motor(4, -50);
     }
-    motor(1, 50);
-    delay(1500);
-    ao();
-    delay(150);
-    servo(1, 0);
-    //move this package into failed storage
+    delay(del_m);
+    motor(4, 0);
+//    while (read_ir(A1) < 12) {
+//      motor(4, -50);
+//    }
+//    delay(10);
+//    motor(4, 0);
+    delay(500);
+    servo(1, 30);
+    delay(750);
+    servo(1, 90);
   }
 }
 
@@ -153,43 +139,53 @@ void printResultF(HUSKYLENSResult result) {
   glcd(2, 0, "W = %d ", result.height);
   glcd(3, 0, "H = %d ", result.width);
   float falseDim = result.width * result.height;
-  for (int i = 0; i < on.countBlocks(index); i++) {
-    HUSKYLENSResult result = on.get(index, i);
+  for (int i = 0; i < on.countBlocks(1); i++) {
+    HUSKYLENSResult result = on.get(1, i);
     dim = result.height * result.width;
   }
-  float newFalse = dim - falseDim;
+  float newFalse = abs(dim - falseDim);
   float falsePercentage = (newFalse / fullyDimension) * 100;
+  float true_area = (falseDim / fullyDimension) * 100;
+  true_area = 100 - true_area;
+  //  if(falsePercentage > truePercentage){
+  //    float sum = falsePercentage - truePercentage;
+  //    glcd(7, 0, "Total area= %f% ", sum);
+  //  }else{
+  //    float sum1 = truePercentage - falsePercentage;
+  //    glcd(7, 0, "Total area= %f% ", sum1);
+  //  }
+  //float newArea = 100 - falsePercentage;
   glcd(6, 0, "Dimension= %f ", falseDim);
-  glcd(7, 0, "Total area= %f% ", falsePercentage);
+  glcd(7, 0, "Total area= %f% ", true_area);
   if (falsePercentage > 80.00) {
     swnd('P');
     Serial.print("Pass");
     mySerial.write('P');
-    if (sdChk == true) {
-      passed_package += 1;
-      //move package into passed storage
-      while (read_ir(A1) > 12) {
-        motor(1, 50);
-      }
-      //move package into passed storage
+    passed_package += 1;
+    while (read_ir(A2) > 12) {
+      motor(4, -50);
     }
+    delay(10);
+    motor(4, 0);
   } else {
     swnd('F');
     Serial.print("Failed");
-    servo(1, 50);
-    delay(600);
     mySerial.write('F');
     failed_package += 1;
-    //move this package into failed storage
     while (read_ir(A1) > 12) {
-      motor(1, 50);
+      motor(4, -50);
     }
-    motor(1, 50);
-    delay(1500);
-    ao();
-    delay(150);
-    servo(1,0);
-    //move this package into failed storage
+//    delay(10);
+//    motor(4, 0);
+//    while (read_ir(A1) < 12) {
+//      motor(4, -50);
+//    }
+    delay(del_m);
+    motor(4, 0);
+    delay(500);
+    servo(1, 30);
+    delay(750);
+    servo(1, 90);
   }
 }
 
